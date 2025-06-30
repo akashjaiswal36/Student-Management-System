115f93b25d741946eb69dabc188dfa74016af3dd
Akash Jaiswal <akash.jaiswal@colruytgroup.com>
2025-06-30 21:50:40 +0200

    added test folder

--- src/main/java/com/example/student/StudentController.java
+++ src/main/java/com/example/student/StudentController.java
@@ -1,22 +1,39 @@
 package com.example.student;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController                   // JSON API
 @RequestMapping("/api/students") // base URL
 public class StudentController {
-    private final StudentService service;
-    public StudentController(StudentService s) { this.service = s; }
 
-    @GetMapping                public List<Student> all()        { return service.findAll(); }
-    @GetMapping("/{id}")       public Student one(@PathVariable int id) {
-                                   return service.findById(id);
-                               }
-    @PostMapping               public Student add(@RequestBody Student s) {
-                                   service.save(s); return s;
-                               }
-    @DeleteMapping("/{id}")    public void delete(@PathVariable int id) {
-                                   service.delete(id);
-                               }
+    private final StudentService service;
+
+    public StudentController(StudentService s) {
+        this.service = s;
+    }
+
+    @GetMapping
+    public List<Student> all() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Student one(@PathVariable int id) {
+        return service.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student add(@RequestBody Student s) {
+        service.save(s);
+        return s;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id) {
+        service.delete(id);
+    }
 }
+
